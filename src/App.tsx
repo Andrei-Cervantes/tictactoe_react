591174e1ce3cb083b9715602f8be76a3ff190645
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-06-12 23:46:04 +0800

    style: add background color theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Game from "./components/Game";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-slate-900 to-slate-800">
       <Game />
     </div>
   );
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,15 @@
+import Board from "./Board";
+import ResetButton from "./ResetButton";
+import Status from "./Status";
+
 const Game = () => {
-  return <div>Game</div>;
+  return (
+    <div className="space-y-4 p-6 bg-slate-700 rounded-xl shadow-2xl w-[300px] text-center border border-cyan-400/30">
+      <Status />
+      <Board />
+      <ResetButton />
+    </div>
+  );
 };
 
 export default Game;
