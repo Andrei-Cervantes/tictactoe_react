31756e6ed0b8b60d643cc2b71eff7e973307a5ed
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-06-15 00:33:13 +0800

    refactor: improve background component

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,9 +1,39 @@
-import background from "../assets/background.svg";
+import BackgroundSVG, { colorThemes } from "./BackgroundSVG";
 
-const Background = () => {
+interface BackgroundProps {
+  primaryColor?: string;
+  secondaryColor?: string;
+  className?: string;
+  theme?: keyof typeof colorThemes;
+}
+
+const Background = ({
+  primaryColor,
+  secondaryColor,
+  className = "absolute bottom-0 -left-2 w-1/2",
+  theme = "default",
+}: BackgroundProps) => {
+  // Determine colors to use
+  const colors = (() => {
+    // If both custom colors provided, use them
+    if (primaryColor && secondaryColor) {
+      return { primaryColor, secondaryColor };
+    }
+
+    // If only one custom color provided, warn and use theme
+    if (primaryColor || secondaryColor) {
+      console.warn(
+        "Both primaryColor and secondaryColor must be provided. Falling back to theme."
+      );
+    }
+
+    // Use theme colors (theme is guaranteed to be a valid key)
+    return colorThemes[theme];
+  })();
+
   return (
-    <div className="absolute bottom-0 -left-4">
-      <img src={background} alt="background" className="w-1/2" />
+    <div className={className}>
+      <BackgroundSVG {...colors} className="w-1/2 h-1/2" />
     </div>
   );
 };
