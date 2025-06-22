01c3820f4d550f5c6a3401535f855d6f5cd28f4d
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-06-23 07:23:45 +0800

    refactor: remove background when in mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import useMobile from "./hooks/useMobile";
-import Desktop from "./views/Desktop";
-import Mobile from "./views/Mobile";
+import Background from "./components/Background";
+import { Particles } from "./components/ui/particles";
+import Game from "./components/Game";
 
 const App = () => {
   const isMobile = useMobile();
 
-  return isMobile ? <Mobile /> : <Desktop />;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-primary font-family-primary relative overflow-hidden">
+      <Game />
+      {!isMobile && <Background />}
+      <Particles className="absolute inset-0 z-5" />
+    </div>
+  );
 };
 
 export default App;
