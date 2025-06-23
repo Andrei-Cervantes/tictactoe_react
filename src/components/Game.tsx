24028cfe769fc4514236af54937da9ee56a6d451
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-06-23 23:44:03 +0800

    wip: add handler for getting class names depending on device used

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,6 +3,7 @@ import ResetButton from "./ResetButton";
 import Status from "./Status";
 import WinnerDialog from "./WinnerDialog";
 import { useGameLogic } from "../hooks/useGameLogic";
+import useMobile from "@/hooks/useMobile";
 
 const Game = () => {
   const {
@@ -16,8 +17,21 @@ const Game = () => {
     isGameOver,
   } = useGameLogic();
 
+  const isMobile = useMobile();
+
+  // handle mobile screens
+  const getClassName = () => {
+    if (isMobile) {
+      return "";
+    } else {
+      return "";
+    }
+  };
+
   return (
-    <div className="space-y-12 px-12 py-8 bg-board-bg rounded-xl shadow-2xl w-fit text-center z-15">
+    <div
+      className={`space-y-12 px-12 py-8 bg-board-bg rounded-xl shadow-2xl w-fit text-center z-15 ${getClassName()}`}
+    >
       <Status scores={scores} currentPlayer={currentPlayer} />
       <Board board={board} onSquareClick={handleSquareClick} />
       <ResetButton onReset={handleReset} />
